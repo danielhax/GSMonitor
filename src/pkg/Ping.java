d462a881d95fae4c69343b2913dda883204d9308
danielhax <ddvrns@gmail.com>
2016-06-26 23:37:07 +0800

    Output average of last 20 ping

--- src/pkg/Ping.java
+++ src/pkg/Ping.java
@@ -6,6 +6,7 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -23,11 +24,17 @@ public class Ping extends Thread{
     private int rtoCount;
     private float averageRTO;
     private String ip;
+    private Integer[] last20Pings;
+    private int iterator;
 
     public Ping(String ip) throws IOException {
         this.ip = ip;                               //use the inputted ip as a local string to be used in this class
         lineCount = rtoCount = 0;                   //reset total lines and packet loss average every object creation
         p = rt.exec("ping " + ip + " /t");          //runtime command: ping *ip address* -t
+
+        last20Pings = new Integer[20];   //each ping is stored here
+        Arrays.fill(last20Pings, null);  //fill array with null values
+        iterator = 0;                 //iterator for 'last20Pings' array
     }
 
 
@@ -40,6 +47,7 @@ public class Ping extends Thread{
             while ((line = input.readLine()) != null) {
                 System.out.println(printResult(line));
                 lineCount++;
+
                 for (int i = 0; i < 3; ++i) System.out.println("\r \n");    //clear screen
             }
         } catch (IOException e) {
@@ -49,16 +57,25 @@ public class Ping extends Thread{
 
     private static final String pingToken = "time="; //this is the prefix to the actual integer result, which is the ping
     private String ping;
+    private int averagePing;
 
     private final String printResult(String lineToProcess){
 
         if(lineToProcess.contains("Request timed out."))
             rtoCount++;
         else{
-            for (String word: lineToProcess.split(" ")){
+            for (String word : lineToProcess.split(" ")){
 
-                if(word.contains(pingToken))        // if 'time=' is found
+                if(word.contains(pingToken)) {        // if 'time=' is found
                     ping = word.substring(word.indexOf("=") + 1, word.indexOf("ms"));   //actual ping is after the character '=' and before 'ms'
+
+                    if(iterator != 20)
+                        last20Pings[iterator++] = Integer.parseInt(ping);
+                    else
+                        iterator = 0;
+                    averagePing = averagePing(last20Pings);
+
+                }
                 else
                     continue;
             }
@@ -72,18 +89,33 @@ public class Ping extends Thread{
          */
         return  "Pinging " + ip
                 + "\nTotal Lines: " + lineCount
-                + "\nPing: " + ping
+                + "\nPing: " + averagePing
                 + "\nTime Out Count: " + rtoCount
                 + "\nPacket Loss: " + ((float)Math.round(averageRTO * 100)) + "%";
     }
 
-    private final int averagePing(){
+    private final int averagePing(Integer[] pingsArray){
 
-        /*
-            NEXT GOAL
-            CALCULATE AVERAGE OF LAST 20 PINGS
-         */
+        int i = 0; //while iterator
+        int totalPing = 0;
 
-        return 0;
+        //show pings
+
+        for(Integer j : pingsArray)
+            System.out.print(j + " ");
+
+        System.out.println(i);
+
+        while(i < pingsArray.length){
+            if(pingsArray[i] != null)
+                totalPing += pingsArray[i++];
+            else
+                break;
+        }
+
+
+        System.out.println(i);
+
+        return totalPing / i;
     }
 }
